d82bc84cd0fce38a76d19bb96b041952d96105b1
momonala <mnalavadi@gmail.com>
2023-08-24 11:50:50 +0200

    add args

--- usc/one_off.py
+++ usc/one_off.py
@@ -1,9 +1,14 @@
-from usc.main import monthly_checkin_pipeline
+import argparse
 
+from usc.main import monthly_checkin_pipeline
 from usc.process import get_total_check_ins_for_msg
 from usc.telegram import send_to_telegram
 
 if __name__ == '__main__':
-    monthly_checkin_pipeline(pages=50)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pages", default=50, type=int, help="number of checkin pages to look through")
+    args = parser.parse_args()
+
+    monthly_checkin_pipeline(pages=args.pages)
     msg = get_total_check_ins_for_msg()
     send_to_telegram(msg)
